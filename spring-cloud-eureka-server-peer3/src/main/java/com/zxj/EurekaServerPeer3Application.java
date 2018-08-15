5b3b28f17038a4bb1779eccdbebe810ace2d2fdf
zjls007 <zjls007@qq.com>
2018-08-15 10:58:24 +0800

    eureka注册中心集群

---
+++ spring-cloud-eureka-server-peer3/src/main/java/com/zxj/EurekaServerPeer3Application.java
@@ -0,0 +1,19 @@
+package com.zxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zxj
+ * @create 2018/8/15 10:14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerPeer3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerPeer3Application.class, args);
+    }
+
+}
