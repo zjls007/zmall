56eba0dd6ae72db1acf52d6e3ab24d9885084d19
zjls007 <zjls007@qq.com>
2018-08-15 14:02:54 +0800

    服务提供者

---
+++ spring-cloud-service-provider-user-cluster2/src/main/java/com/zxj/UserProviderCluster2Application.java
@@ -0,0 +1,33 @@
+package com.zxj;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zxj
+ * @create 2018/8/15 13:41
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class UserProviderCluster2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProviderCluster2Application.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public Object home() {
+        return "port: " + port;
+    }
+
+}
